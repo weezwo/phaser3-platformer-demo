14195048cc31e50bcf1d0eb99b44895299b98cf3
Luisa Scavo <luisascavo@gmail.com>
2020-05-27 21:06:02 -0400

    add assets

---
+++ assets/tilesets/environment.tsx
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="environment" tilewidth="64" tileheight="80" tilecount="37" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="48" source="../environment/sliced-tileset/ground-b.png"/>
+ </tile>
+ <tile id="1">
+  <image width="48" height="32" source="../environment/sliced-tileset/ground-corner.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="32" source="../environment/sliced-tileset/ground-right-corner.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="32" source="../environment/sliced-tileset/ground-shadow.png"/>
+ </tile>
+ <tile id="4">
+  <image width="48" height="32" source="../environment/sliced-tileset/ground-wall-b.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="../environment/sliced-tileset/ground-wall.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="16" source="../environment/sliced-tileset/ground-wood-legs.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="48" source="../environment/sliced-tileset/ground.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="../environment/sliced-tileset/left-corner.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="80" source="../environment/sliced-tileset/left-roof.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="32" source="../environment/sliced-tileset/left-slope.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="32" source="../environment/sliced-tileset/right-corner.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="80" source="../environment/sliced-tileset/right-roof.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="32" source="../environment/sliced-tileset/right-slope.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="80" source="../environment/sliced-tileset/roof.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="32" source="../environment/sliced-tileset/shadow.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="32" source="../environment/sliced-tileset/slope.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="32" source="../environment/sliced-tileset/stairs-left.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="32" source="../environment/sliced-tileset/stairs-mirror-left.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="32" source="../environment/sliced-tileset/stairs-mirror-right.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="32" source="../environment/sliced-tileset/stairs-mirror.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="32" source="../environment/sliced-tileset/stairs-right.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="32" source="../environment/sliced-tileset/stairs-wall-left.png"/>
+ </tile>
+ <tile id="23">
+  <image width="16" height="32" source="../environment/sliced-tileset/stairs-wall-mirror-left.png"/>
+ </tile>
+ <tile id="24">
+  <image width="16" height="32" source="../environment/sliced-tileset/stairs-wall-mirror-right.png"/>
+ </tile>
+ <tile id="25">
+  <image width="16" height="32" source="../environment/sliced-tileset/stairs-wall-mirror.png"/>
+ </tile>
+ <tile id="26">
+  <image width="16" height="32" source="../environment/sliced-tileset/stairs-wall-right.png"/>
+ </tile>
+ <tile id="27">
+  <image width="16" height="32" source="../environment/sliced-tileset/stairs-wall.png"/>
+ </tile>
+ <tile id="28">
+  <image width="16" height="32" source="../environment/sliced-tileset/stairs.png"/>
+ </tile>
+ <tile id="29">
+  <image width="32" height="32" source="../environment/sliced-tileset/top-left-wood.png"/>
+ </tile>
+ <tile id="30">
+  <image width="32" height="32" source="../environment/sliced-tileset/top-right-wood.png"/>
+ </tile>
+ <tile id="31">
+  <image width="16" height="16" source="../environment/sliced-tileset/top-wood.png"/>
+ </tile>
+ <tile id="32">
+  <image width="16" height="16" source="../environment/sliced-tileset/wall-b.png"/>
+ </tile>
+ <tile id="33">
+  <image width="16" height="16" source="../environment/sliced-tileset/wall.png"/>
+ </tile>
+ <tile id="34">
+  <image width="32" height="64" source="../environment/sliced-tileset/window-bars.png"/>
+ </tile>
+ <tile id="35">
+  <image width="32" height="64" source="../environment/sliced-tileset/window.png"/>
+ </tile>
+ <tile id="36">
+  <image width="32" height="16" source="../environment/sliced-tileset/wood-legs.png"/>
+ </tile>
+</tileset>
---
+++ assets/tilesets/props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="props" tilewidth="32" tileheight="32" tilecount="66" columns="11">
+ <image source="../props.png" width="352" height="192"/>
+</tileset>
