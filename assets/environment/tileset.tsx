27bfbd1bf6b59f8468435c7db9717e30d0fb68df
Luisa Scavo <luisascavo@gmail.com>
2020-05-29 23:30:38 -0400

    add ground and camera tracking(!)

---
+++ assets/environment/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="tileset" tilewidth="16" tileheight="16" tilecount="444" columns="37">
+ <image source="tileset.png" width="592" height="192"/>
+</tileset>
--- assets/tilesets/props.tsx
+++ assets/tilesets/props.tsx
@@ -1,4 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="props" tilewidth="32" tileheight="32" tilecount="66" columns="11">
- <image source="../props.png" width="352" height="192"/>
+<tileset version="1.2" tiledversion="1.3.4" name="props" tilewidth="221" tileheight="244" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="24" height="30" source="../props/barrel.png"/>
+ </tile>
+ <tile id="1">
+  <image width="73" height="68" source="../props/crate-stack.png"/>
+ </tile>
+ <tile id="2">
+  <image width="39" height="35" source="../props/crate.png"/>
+ </tile>
+ <tile id="3">
+  <image width="168" height="183" source="../props/house-a.png"/>
+ </tile>
+ <tile id="4">
+  <image width="210" height="244" source="../props/house-b.png"/>
+ </tile>
+ <tile id="5">
+  <image width="221" height="183" source="../props/house-c.png"/>
+ </tile>
+ <tile id="6">
+  <image width="37" height="45" source="../props/sign.png"/>
+ </tile>
+ <tile id="7">
+  <image width="35" height="108" source="../props/street-lamp.png"/>
+ </tile>
+ <tile id="8">
+  <image width="93" height="75" source="../props/wagon.png"/>
+ </tile>
+ <tile id="9">
+  <image width="65" height="65" source="../props/well.png"/>
+ </tile>
 </tileset>
